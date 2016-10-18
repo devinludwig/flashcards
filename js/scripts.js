7302115a194c813f6183df19bae4168fa0a43e3d
Adam Baker and Devin Ludwig <devin@>
2016-10-18 15:03:13 -0700

    display flashcards and definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,75 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".term").toggle();
-    $(".definition").toggle();
+  $("#javascriptterm, #javascriptdef").click(function() {
+    $("#javascriptterm").toggle();
+    $("#javascriptdef").toggle();
   });
+  $("#operatorsterm, #operatorsdef").click(function() {
+    $("#operatorsterm").toggle();
+    $("#operatorsdef").toggle();
+  });
+  $("#variablesterm, #variablesdef").click(function() {
+    $("#variablesterm").toggle();
+    $("#variablesdef").toggle();
+  });
+  $("#varnamingterm, #varnamingdef").click(function() {
+    $("#varnamingterm").toggle();
+    $("#varnamingdef").toggle();
+  });
+  $("#functionsterm, #functionsdef").click(function() {
+    $("#functionsterm").toggle();
+    $("#functionsdef").toggle();
+  });
+  $("#methodsterm, #methodsdef").click(function() {
+    $("#methodsterm").toggle();
+    $("#methodsdef").toggle();
+  });
+  $("#parametersterm, #parametersdef").click(function() {
+    $("#parametersterm").toggle();
+    $("#parametersdef").toggle();
+  });
+  $("#returnterm, #returnsdef").click(function() {
+    $("#returnterm").toggle();
+    $("#returnsdef").toggle();
+  });
+  $("#chainingterm, #chainingdef").click(function() {
+    $("#chainingterm").toggle();
+    $("#chainingdef").toggle();
+  });
+  $("#stringsterm, #stringsdef").click(function() {
+    $("#stringsterm").toggle();
+    $("#stringsdef").toggle();
+  });
+  $("#booleansterm, #booleansdef").click(function() {
+    $("#booleansterm").toggle();
+    $("#booleansdef").toggle();
+  });
+  $("#undefinedterm, #undefineddef").click(function() {
+    $("#undefinedterm").toggle();
+    $("#undefineddef").toggle();
+  });
+  $("#nanterm, #nandef").click(function() {
+    $("#nanterm").toggle();
+    $("#nandef").toggle();
+  });
+  $("#escapeterm, #escapedef").click(function() {
+    $("#escapeterm").toggle();
+    $("#escapedef").toggle();
+  });
+  $("#alertterm, #alertdef").click(function() {
+    $("#alertterm").toggle();
+    $("#alertdef").toggle();
+  });
+  $("#commentsterm, #commentsdef").click(function() {
+    $("#commentsterm").toggle();
+    $("#commentsdef").toggle();
+  });
+  $("#jqueryterm, #jquerydef").click(function() {
+    $("#jqueryterm").toggle();
+    $("#jquerydef").toggle();
+  });
+  $("#attributesterm, #attributesdef").click(function() {
+    $("#attributesterm").toggle();
+    $("#attributesdef").toggle();
+  });
+
 });
